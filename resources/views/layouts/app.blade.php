b4443acd2bb94ede63b6cbe344cb615a8432c4dc
rad129ratul <ratulrs29@gmail.com>
2025-10-28 00:01:05 +0600

    commit 26

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,26 +10,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     
-    @if(app()->environment('production'))
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @if(file_exists(public_path('build/manifest.json')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     @else
-        @vite(['resources/css/app.css'])
+        <link rel="stylesheet" href="{{ asset('build/assets/app-EFjBssnp.css') }}">
     @endif
 </head>
 <body class="bg-dark text-light">
     @include('components.navbar')
+    
     <main class="container-fluid px-4 py-4">
         @include('components.alerts')
+        
         @yield('content')
     </main>
 
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     
-    @if(app()->environment('production'))
-        <script src="{{ asset('js/app.js') }}"></script>
-    @else
-        @vite(['resources/js/app.js'])
+    @if(!file_exists(public_path('build/manifest.json')))
+        <script src="{{ asset('build/assets/app-Bj43h_rG.js') }}"></script>
     @endif
     
     @stack('scripts')
