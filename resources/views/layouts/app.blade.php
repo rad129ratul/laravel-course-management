bcb798c3f4c106e84fd4271f6a2fea4170944503
rad129ratul <ratulrs29@gmail.com>
2025-10-27 17:47:13 +0600

    commit 16

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,22 +10,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     
-    <!-- Replace asset() with vite() directive -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    
     @include('components.navbar')
-
     <main class="container-fluid px-4 py-4">
         @include('components.alerts')
-        
         @yield('content')
     </main>
-
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    
     @stack('scripts')
 </body>
 </html>
