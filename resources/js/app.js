1e3587223d677aa1f3da667efafb165393a2d815
rad129ratul <ratulrs29@gmail.com>
2025-10-27 21:51:52 +0600

    commit 17

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import 'bootstrap';
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Course Management System loaded');
+    
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,16 +10,28 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @if(app()->environment('production'))
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @else
+        @vite(['resources/css/app.css'])
+    @endif
 </head>
-<body>
+<body class="bg-dark text-light">
     @include('components.navbar')
     <main class="container-fluid px-4 py-4">
         @include('components.alerts')
         @yield('content')
     </main>
+
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    
+    @if(app()->environment('production'))
+        <script src="{{ asset('js/app.js') }}"></script>
+    @else
+        @vite(['resources/js/app.js'])
+    @endif
+    
     @stack('scripts')
 </body>
 </html>
