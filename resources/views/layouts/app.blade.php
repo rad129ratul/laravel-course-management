722f98c1fe39c689b406ef9597f7c177b6dd18d5
rad129ratul <ratulrs29@gmail.com>
2025-10-27 17:37:54 +0600

    commit 15

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,9 @@
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    <!-- Replace asset() with vite() directive -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     
